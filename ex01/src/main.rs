26315b031ff3cab5ab5d8e90c4953a8bf7492641
Rohit Patil <rohit.patil.wrc.555@gmail.com>
2023-01-08 08:43:37 +0000

    Updated example for stm32

--- ex01/src/main.rs
+++ ex01/src/main.rs
@@ -4,9 +4,15 @@
 use core::panic::PanicInfo;
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn Reset() -> ! {
     loop {}
 }
+
+// The reset vector, a pointer into the reset handler
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
