3ecd7e827b63973802f2b56e03f3f8f807b8ce27
Rohit Patil <rohit.patil.wrc.555@gmail.com>
2023-01-16 17:05:31 +0000

    example 3 - adding few more methods

--- ex03/intf/src/lib.rs
+++ ex03/intf/src/lib.rs
@@ -1,6 +1,19 @@
+use core::slice::from_raw_parts;
 
 #[no_mangle]
 pub extern "C" fn rust_hello() {
 	println!("Hello World from Rust !!");
 }
 
+#[no_mangle]
+pub extern "C" fn rust_add(a: u32, b: u32) -> u32 {
+	a + b
+}
+
+#[no_mangle]
+pub extern "C" fn rust_array(a:*mut u32, c: u32) {
+	let a = unsafe{ from_raw_parts( a, c as usize) };
+	for (i, x) in a.iter().enumerate() {
+		println!("Array {} => {:x}", i, x);
+	}
+}
--- ex03/main.c
+++ ex03/main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdint.h>
 
 // Rust proto
 void rust_hello(void);
+uint32_t rust_add(uint32_t a, uint32_t b);
+void rust_array(uint32_t *p, uint32_t count);
 
 int main()
 {
 	rust_hello();
+
+	// call rust add api
+	uint32_t c = rust_add( 2, 3);
+	printf("2 + 3 = %d \n", c);
+
+	// passing array to rust function.
+	uint32_t arr[5] = { 0x11, 0x22, 0x33, 0x44, 0x55 };
+	rust_array( arr, 5);
+
+	// passing structure to rust function.
+
+	// passing enum to rust function.
+
 	return 0;
 }
