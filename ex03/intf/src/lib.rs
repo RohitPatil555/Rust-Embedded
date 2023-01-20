03eb2ef71eb534ab0d941e0adde7ab29c27d0d7b
Rohit Patil <rohit.patil.wrc.555@gmail.com>
2023-01-20 18:18:01 +0000

    Ex03 - Structure and enum handling added

--- ex03/intf/src/lib.rs
+++ ex03/intf/src/lib.rs
@@ -1,4 +1,18 @@
 use core::slice::from_raw_parts;
+use core::str::from_utf8;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct MyInfo {
+	a: u32,
+	b: u32
+}
+
+#[repr(C)]
+pub enum MyEnum {
+	Hello = 1,
+	Bye
+}
 
 #[no_mangle]
 pub extern "C" fn rust_hello() {
@@ -15,5 +29,24 @@ pub extern "C" fn rust_array(a:*mut u32, c: u32) {
 	let a = unsafe{ from_raw_parts( a, c as usize) };
 	for (i, x) in a.iter().enumerate() {
 		println!("Array {} => {:x}", i, x);
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn rust_string(a:*mut char, c: u32) {
+	let s = from_utf8(unsafe{from_raw_parts(a as *mut u8, c as usize)}).unwrap();
+	println!("Got string {}", s);
+}
+
+#[no_mangle]
+pub extern "C" fn rust_struct_print(p:*mut MyInfo) {
+	println!("{:x?}", unsafe { p.as_ref() }.unwrap() );
+}
+
+#[no_mangle]
+pub extern "C" fn rust_enum_print(v: MyEnum) {
+	match v {
+		MyEnum::Hello => println!("Hello from C"),
+		MyEnum::Bye => println!("Bye from C"),
 	}
 }
--- ex03/main.c
+++ ex03/main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+
+typedef struct {
+	uint32_t a;
+	uint32_t b;
+} myinfo_t;
+
+typedef enum {
+	HELLO = 1,
+	BYE
+} myenum_t;
 
 // Rust proto
 void rust_hello(void);
 uint32_t rust_add(uint32_t a, uint32_t b);
 void rust_array(uint32_t *p, uint32_t count);
+void rust_string(char *p, uint32_t count);
+void rust_struct_print(myinfo_t * ptr);
+void rust_enum_print(myenum_t val);
 
 int main()
 {
@@ -18,9 +32,20 @@ int main()
 	uint32_t arr[5] = { 0x11, 0x22, 0x33, 0x44, 0x55 };
 	rust_array( arr, 5);
 
+	// passing string to rust
+	char * s = "Yes, got me";
+	rust_string( s, strlen(s));
+
 	// passing structure to rust function.
+	myinfo_t m = { 0x66, 0x77 };
+	rust_struct_print( &m );	
 
 	// passing enum to rust function.
+	myenum_t v1 = HELLO;
+	rust_enum_print(v1);
+
+	v1 = BYE;
+	rust_enum_print(v1);
 
 	return 0;
 }
