41ccb4cc70aa1279385284bf96dfd661eba15ff1
Rohit Patil <rohit.patil.wrc.555@gmail.com>
2024-03-08 17:33:05 +0000

    06: Add support for event,state and default keywords

--- ex09/src/custom_parse.rs
+++ ex09/src/custom_parse.rs
@@ -1,4 +1,4 @@
-use crate::custom_token::{context, sm_name};
+use crate::custom_token::{context, default, event, sm_name, state};
 use syn::{
     braced,
     parse::{Parse, ParseStream},
@@ -10,6 +10,15 @@ use syn::{
 pub(crate) struct StateMachine {
     pub name: String,
     pub context_fields: Punctuated<Field, Token![,]>,
+    pub event_list: Vec<SmacEvent>,
+    pub state_list: Vec<String>,
+    pub state_default: String,
+}
+
+#[derive(Default)]
+pub(crate) struct SmacEvent {
+    pub name: String,
+    pub evt_fields: Punctuated<Field, Token![,]>,
 }
 
 impl Parse for StateMachine {
@@ -32,7 +41,43 @@ impl Parse for StateMachine {
             smac.context_fields = content.parse_terminated(Field::parse_named, Token![,])?;
         }
 
+        while input.peek(event) {
+            let evt = input.parse::<SmacEvent>()?;
+            smac.event_list.push(evt);
+        }
+
+        while input.peek(state) {
+            let _ = input.parse::<state>()?;
+            let state_name: LitStr = input.parse()?;
+            smac.state_list.push(state_name.value());
+        }
+
+        if input.peek(default) {
+            let _ = input.parse::<default>()?;
+            let state_name: LitStr = input.parse()?;
+
+            smac.state_default = state_name.value();
+        }
+
         Ok(smac)
+    }
+}
+
+impl Parse for SmacEvent {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let mut evt: SmacEvent = SmacEvent::default();
+
+        if input.peek(event) {
+            let _ = input.parse::<event>()?;
+            let name: LitStr = input.parse()?;
+            let content;
+            let _ = braced!(content in input);
+
+            evt.name = name.value();
+            evt.evt_fields = content.parse_terminated(Field::parse_named, Token![,])?;
+        }
+
+        Ok(evt)
     }
 }
 
@@ -69,5 +114,73 @@ mod tests {
         for field in smac.context_fields {
             println!("{:?}", field);
         }
+    }
+
+    #[test]
+    fn parse_event_1() {
+        let input = "event \"TestEvent\" { dd: u8, }";
+
+        let smac = syn::parse_str::<StateMachine>(input).unwrap();
+
+        for evt in &smac.event_list {
+            println!("Event name : {}", evt.name);
+            assert_eq!("TestEvent", evt.name);
+        }
+
+        assert_eq!(1, smac.event_list.len(), "Single Event not found");
+    }
+
+    #[test]
+    fn parse_event_2() {
+        let input = "event \"TestEvent1\" { dd: u8, } 
+                    event \"TestEvent2\" { tt: u8, }";
+        let mut idx: u8 = 1;
+
+        let smac = syn::parse_str::<StateMachine>(input).unwrap();
+
+        for evt in &smac.event_list {
+            println!("Event name : {}", evt.name);
+            assert_eq!(format!("TestEvent{}", idx), evt.name);
+            idx += 1;
+        }
+
+        assert_eq!(2, smac.event_list.len(), "Single Event not found");
+    }
+
+    #[test]
+    fn parse_state_1() {
+        let input = "state \"TestState1\"";
+
+        let smac = syn::parse_str::<StateMachine>(input).unwrap();
+
+        for name in &smac.state_list {
+            assert_eq!("TestState1", name);
+        }
+
+        assert_eq!(1, smac.state_list.len());
+    }
+
+    #[test]
+    fn parse_state_2() {
+        let input = "state \"TestState1\"
+                     state \"TestState2\"";
+        let mut idx: u8 = 1;
+
+        let smac = syn::parse_str::<StateMachine>(input).unwrap();
+
+        for name in &smac.state_list {
+            assert_eq!(format!("TestState{}", idx), *name);
+            idx += 1;
+        }
+
+        assert_eq!(2, smac.state_list.len());
+    }
+
+    #[test]
+    fn parse_default() {
+        let input = "default \"TestStateDef\"";
+        let smac = syn::parse_str::<StateMachine>(input).unwrap();
+
+        assert_eq!("TestStateDef", smac.state_default);
     }
 }
--- ex09/src/custom_token.rs
+++ ex09/src/custom_token.rs
@@ -1,2 +1,5 @@
 syn::custom_keyword!(sm_name);
 syn::custom_keyword!(context);
+syn::custom_keyword!(event);
+syn::custom_keyword!(state);
+syn::custom_keyword!(default);
